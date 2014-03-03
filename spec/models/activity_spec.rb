34d6ca0db40f11e3f0688055007383e518267818
Laure Fischer <laurefischer27@gmail.com>
2014-03-03 10:30:39 -0500

    added has many through associations for activity model

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -4,5 +4,7 @@ describe Activity do
   describe "associations" do
     it { should belong_to :user }
     it { should have_many :adventures }
+    it { should have_many(:users).through(:adventures) }
+    it { should have_many(:stories).through(:adventures) }
   end
 end
