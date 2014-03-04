b645dab322c3a364622af1625ef3f24f54067544
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-04 00:06:51 -0500

    Form for new activity validates user input and redirects to sign in for visitors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if session[:activity_params].present?
+      session[:activity_params][:user_id] = current_user.id
+      @activity = Activity.new(session[:activity_params])
+      @activity.save
+      activity_path(@activity)
+    else
+      session[:previous_url] || root_path
+    end
+  end
 end
