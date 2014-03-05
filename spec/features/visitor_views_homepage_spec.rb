222a4851e82836dab10b1ac601fcd9ec391221f2
Laure Fischer <laurefischer27@gmail.com>
2014-03-05 10:08:05 -0500

    edited spec for visitor viewing homepage to match html

--- spec/features/visitor_views_homepage_spec.rb
+++ spec/features/visitor_views_homepage_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Visitor views homepage' do
   scenario 'displays home screen with button' do
     visit root_path
-    expect(page).to have_content("Do Me!")  # App Title
+    expect(page).to have_content("Do me!")  # App Title
     expect(page).to have_link("About")
     expect(page).to have_link("Suggest an activity")
     expect(page).to have_link("Sign in")
