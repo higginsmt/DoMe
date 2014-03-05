7874191b4a9843e7413829bda614d302ba4fcc29
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-04 21:55:14 -0500

    show page for adventure now shows the activity and stories (form in progress)

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -6,10 +6,16 @@ class AdventuresController < ApplicationController
   def new
     @adventure = Adventure.new(user_id: current_user.id, activity_id: params[:activity_id])
     @adventure.save
-    render json: @adventure # what does this line do?
+    render json: @adventure # because this action started in ajax, it automatically goes back to ajax, and the redirect must be done from there
   end
 
   def update
+    @adventure = Adventure.where(user_id: current_user.id, activity_id: params[:id]).first # the .first is if "did" the activity more than once
+    @activity = Activity.find(params[:id])
+    all_adventures = Adventure.where(activity_id: params[:id]).all
+    @stories = all_adventures.map do |adventure|
+      adventure[:story]
+    end
   end
 
 end
